e87e92f8a22362afc7584d854cf2347d83490f6f
github-action-benchmark <github@users.noreply.github.com>
2026-02-07 21:04:19 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 4b0c39243afe02b26eae8a267c12b8f8bc08c057

---
+++ benchmarks/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1770498259614,
+  "repoUrl": "https://github.com/matheusmelo-cabelo/n8n-mcp-universal",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "backup.matheusmelo@gmail.com",
+            "name": "matheusmelo-cabelo",
+            "username": "matheusmelo-cabelo"
+          },
+          "committer": {
+            "email": "backup.matheusmelo@gmail.com",
+            "name": "matheusmelo-cabelo",
+            "username": "matheusmelo-cabelo"
+          },
+          "distinct": true,
+          "id": "4b0c39243afe02b26eae8a267c12b8f8bc08c057",
+          "message": "feat: initial commit of n8n-MCP Universal with Gemini skills and SSE support",
+          "timestamp": "2026-02-07T17:57:20-03:00",
+          "tree_id": "df27f5d5528426bab01a1c8dcf8b32dce5b66a6e",
+          "url": "https://github.com/matheusmelo-cabelo/n8n-mcp-universal/commit/4b0c39243afe02b26eae8a267c12b8f8bc08c057"
+        },
+        "date": 1770498259232,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
