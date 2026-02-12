46f45c93bafb9771788bce9c7b571f89af34857e
github-action-benchmark <github@users.noreply.github.com>
2026-02-12 05:30:17 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for a1bb5c1696e5a3fa95deb3f98906d96ba4c961bc

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770498259614,
+  "lastUpdate": 1770874217915,
   "repoUrl": "https://github.com/matheusmelo-cabelo/n8n-mcp-universal",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -23,6 +23,37 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/matheusmelo-cabelo/n8n-mcp-universal/commit/4b0c39243afe02b26eae8a267c12b8f8bc08c057"
         },
         "date": 1770498259232,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "backup.matheusmelo@gmail.com",
+            "name": "matheusmelo-cabelo",
+            "username": "matheusmelo-cabelo"
+          },
+          "committer": {
+            "email": "backup.matheusmelo@gmail.com",
+            "name": "matheusmelo-cabelo",
+            "username": "matheusmelo-cabelo"
+          },
+          "distinct": true,
+          "id": "a1bb5c1696e5a3fa95deb3f98906d96ba4c961bc",
+          "message": "feat: add render blueprint and dockerfiles for cloud deploy",
+          "timestamp": "2026-02-12T02:28:15-03:00",
+          "tree_id": "bdcbbd23c1cbcea2a69753cca802bb89b6b3e427",
+          "url": "https://github.com/matheusmelo-cabelo/n8n-mcp-universal/commit/a1bb5c1696e5a3fa95deb3f98906d96ba4c961bc"
+        },
+        "date": 1770874217535,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
